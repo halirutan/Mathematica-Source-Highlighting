b96e4240da56c5e3944a5e246f8e059ddb7f5e7f
Patrick <pscheibe@trm.uni-leipzig.de>
2012-05-02 10:36:49 +0200

    Changed name in the license section.

--- lang-mma.js
+++ lang-mma.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2009 Google Inc.
+// Copyright (C) 2012 Patrick Scheibe
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
